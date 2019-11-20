b8189261567c1068c79c078338082968a0a64990
MondaleFelix <mondalefelix@yahoo.com>
2019-11-19 19:21:18 -0800

    sqaure generates once it is out of bounds

--- PopTheSquares/PopTheSquares/GameScene.swift
+++ PopTheSquares/PopTheSquares/GameScene.swift
@@ -26,22 +26,26 @@ class GameScene: SKScene {
         let size = CGSize(width: 80, height: 80)
         let square = SKSpriteNode(texture: nil, color: .green, size: size)
         
+//      Random x axis position
         let randomNumber = CGFloat.random(in: 0 ... self.view!.bounds.width)
 
         square.position.x = randomNumber
         square.position.y = 0
         
+//      Action that moves the square up
         let moveUp = SKAction.customAction(withDuration: 2) { (square, _) in
             square.position.y += 7
+            
+//       Readds sqaure once it out of bounds
+            if square.position.y > self.view!.frame.height {
+                square.removeFromParent()
+                self.newSquare()
+            }
+            
         }
         
         
         square.run(moveUp)
-//                let actionSequence = SKAction.sequence([moveUP])
-    
-//                square.run(SKAction.repeatForever(action))
-        
-
         addChild(square)
     }
 }
